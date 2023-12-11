1a634fdf1d5c005309e19875771d0bcbf65e820a
Marco Andrello <marco.andrello@gmail.com>
2023-12-11 16:27:42 +0100

    lavoro a casa 11 12 2023

--- 00 - studio gstat.R
+++ 00 - studio gstat.R
@@ -1,3 +1,10 @@
+# VARIO CODICE PER INTERPOLAZIONI SPAZIALI
+
+#########################################################################
+# RSPAT E GSPATIAL
+#########################################################################
+
+
 # https://rspatial.org/analysis/4-interpolation.html
 
 remotes::install_github('rspatial/rspat')
@@ -85,6 +92,9 @@ rmsenn
 mean(rmsenn)
 1 - (mean(rmsenn) / null)
 
+#########################################################################
+# spatialRF Moran Eigenvector Maps
+#########################################################################
 
 # Try MEMs
 library(spatialRF)
@@ -111,3 +121,108 @@ sea_dist <-  costDistance(t, fromCoords)
 sea_dist %>% as.matrix -> sea_dist
 m2 <- rf_spatial(data=data, dependent.variable.name = "pca", predictor.variable.name = "dummy",
                 distance.matrix = sea_dist, method="mem.moran.sequential")
+
+
+
+#########################################################################
+# inverse path distance weighting
+#########################################################################
+
+
+rm(list=ls())
+
+library(tidyverse)
+library(sf)
+library(ipdw)
+
+pols <- st_read(system.file("extdata/kattegat_coast.gpkg", package = "ipdw"))
+pnts <- st_read(system.file("extdata/kattegat_pnts.gpkg", package = "ipdw"))
+
+costras <- costrasterGen(pnts, pols, extent = "pnts",
+                         projstr = projection(pols))
+# insert contiguous barrier
+costras[160:170, 1:80] <- 10000
+
+library(spatstat)
+
+W              <- owin(range(c(st_bbox(pnts)["xmin"], st_bbox(pnts)["xmax"])),
+                       range(c(st_bbox(pnts)["ymin"], st_bbox(pnts)["ymax"])))
+kat.pp         <- ppp(st_coordinates(pnts)[,1], st_coordinates(pnts)[,2], window = W)
+mean.neighdist <- mean(nndist(kat.pp))
+
+# grid building
+gridsize       <- mean.neighdist * 2
+grainscale.fac <- gridsize / res(costras)[1]
+gridras        <- aggregate(costras, fact = grainscale.fac)
+gridpol        <- rasterToPolygons(gridras)
+gridpol$value  <- row.names(gridpol)
+
+# spatial join
+fulldataset.over <- sf::st_join(pnts, st_as_sf(gridpol))
+
+# grid selection
+set.seed(2)
+gridlev <- unique(fulldataset.over$value)
+for (i in seq_along(gridlev)) {
+    activesub <- subset(fulldataset.over, fulldataset.over$value == gridlev[i])
+    selectnum <- gdata::resample(seq_len(nrow(activesub)), 1)
+    if (i == 1) {
+        training <- activesub[selectnum, ]
+    } else {
+        training <- rbind(training, activesub[selectnum, ])
+    }
+}
+
+
+validate <- fulldataset.over[!(row.names(fulldataset.over) %in%
+                                   row.names(training)), ]
+plot(costras)
+plot(st_geometry(training), add = TRUE)
+plot(st_geometry(validate), col = "red", add = TRUE)
+
+
+paramlist <- c("salinity")
+final.ipdw <- ipdw(training, costras, range = mean.neighdist * 10, paramlist,
+                   overlapped = TRUE)
+final.ipdw2 <- ipdw(training, costras, range = mean.neighdist*20, paramlist, dist_power = 0.5,
+                    overlapped = TRUE)
+
+plot(final.ipdw, main = "Kattegat salinity (ppt)")
+plot(final.ipdw2, main = "Kattegat salinity (ppt) 2")
+plot(pnts,add=T)
+
+##### ottenere il raster delle distances
+library(sf)
+sf_ob <- data.frame(rnorm(2))
+xy    <- data.frame(x = c(4, 2), y = c(8, 4))
+sf_ob <- st_as_sf(cbind(sf_ob, xy), coords = c("x", "y"))
+
+m <- matrix(NA, 10, 10)
+costras <- raster(m, xmn = 0, xmx = ncol(m), ymn = 0, ymx = nrow(m))
+
+# introduce spatial gradient
+costras[] <- runif(ncell(costras), min = 1, max = 10)
+for (i in 1:nrow(costras)) {
+    costras[i, ] <- costras[i, ] + i
+    costras[, i] <- costras[, i] + i
+}
+
+rstack <- pathdistGen(sf_ob, costras, 100, progressbar = FALSE)
+
+
+
+#########################################################################
+# phylin
+#########################################################################
+library(phylin)
+library(adegenet)
+library(tidyverse)
+library(sf)
+load("Data_for_PCA_Diplodus.RData")
+d.gen <- dist.genpop(xpop)
+xpop@other$xy %>% st_as_sf(coords=c("x","y"),crs=4326) %>% st_distance -> r.dist
+units::drop_units(r.dist)/1000 -> r.dist
+gv <- gen.variogram(r.dist, d.gen)
+plot(gv)
+gv$n
+# but then, to interpolate, we need presence or absence of lineages. we do not have such a thing with SNPs (all are present everywhere)
--- 02b - interpolate.R
+++ 02b - interpolate.R
@@ -6,14 +6,18 @@ library(tidyverse)
 library(terra)
 library(sf)
 library(gstat)
+library(adegenet)
 
 load("Planning_units.RData")
 # Create raster covering the extent of pus (for interpolation)
 pust <- rast(pus, resolution = c(10000, 10000), crs = crs(pus))
+values(pust) <- 1
+# Use only the occupied PUs
+mask(pust, (pus %>% filter(Mullus_surmuletus == 1))) -> pusr
 
 
-load("Data_for_PCA_Diplodus.RData")
-load(paste0(getwd(),"/Results/Diplodus_pca_pop.RData"))
+load("Data_for_PCA_Mullus.RData")
+load("Results/Mullus_spca_2753.RData")
 obs <- st_as_sf(data.frame(pca1=0,
                            lon=xpop@other$xy$x,
                            lat=xpop@other$xy$y),
@@ -28,125 +32,109 @@ RMSE <- function(observed, predicted) {
     sqrt(mean((predicted - observed)^2, na.rm=TRUE))
 }
 
-
-i.axis <- 1
-# Dataframe with observation (pca scores) and X and Y coord
-data <- data.frame(pca=pca$li[,i.axis], X=Diplodus_coord_Albers[,1], Y=Diplodus_coord_Albers[,2])
-datav <- vect(data, geom=c("X", "Y"), crs="ESRI:102013")
-
-# 0 - Null model
-null <- RMSE(data$pca, mean(data$pca))
-null
-
-# 1 - Nearest neighbor
-gs <- gstat(formula=pca~1, locations=~X+Y, data=data, nmax=5, set=list(idp = 0))
-values(pust) <- 1
-mask(pust, (pus %>% filter(Diplodus_sargus == 1))) -> pusr
-nn <- interpolate(pusr, gs, xyNames=c("X", "Y"), debug.level=0)
-plot(nn,1)
-# non ha funzionato, ha interpolato comunque tutti i punti del raster.
-nnmsk <- mask(nn,pus)
-plot(nnmsk, 1)
-points(datav,col=terrain.colors(10))
-
-set.seed(5132015)
-kf <- sample(1:5, nrow(data), replace=TRUE)
-rmse_nn <- rep(NA, 5)
-for (k in 1:5) {
-    test <- data[kf == k, ]
-    train <- data[kf != k, ]
-    gscv <- gstat(formula=pca~1, locations=~X+Y, data=train, nmax=5, set=list(idp = 0))
-    p <- predict(gscv, test, debug.level=0)$var1.pred
-    rmse_nn[k] <- RMSE(test$pca, p)
+null <- perf_nn <- vector()
+rmse_nn <- array(NA,dim=c(40,5))
+nnmsk <- list()
+for (i.axis in 1: 40) {
+    # Dataframe with observation (pca scores) and X and Y coord
+    data <- data.frame(pca=spca$li[,i.axis], X=Diplodus_coord_Albers[,1], Y=Diplodus_coord_Albers[,2])
+    # 0 - Null model
+    null[i.axis] <- RMSE(data$pca, mean(data$pca))
+    null
+    # 1 - Nearest neighbor
+    gs <- gstat(formula=pca~1, locations=~X+Y, data=data, nmax=5, set=list(idp = 0))
+    nn <- interpolate(pust, gs, xyNames=c("X", "Y"), debug.level=0)
+    nnmsk[[i.axis]] <- mask(nn,pus)
+    # k-fold cross-validation
+    set.seed(20231211)
+    kf <- sample(1:5, nrow(data), replace=TRUE)
+    for (k in 1:5) {
+        test <- data[kf == k, ]
+        train <- data[kf != k, ]
+        gscv <- gstat(formula=pca~1, locations=~X+Y, data=train, nmax=5, set=list(idp = 0))
+        p <- predict(gscv, test, debug.level=0)$var1.pred
+        rmse_nn[i.axis, k] <- RMSE(test$pca, p)
+    }
+    # relative model performance
+    perf_nn[i.axis] <- 1 - (mean(rmse_nn[i.axis,]) / null[i.axis])
 }
-mean(rmse_nn)
-# relative model performance
-perf_nn <- 1 - (mean(rmse_nn) / null)
-round(perf_nn, 3)
 
-# 2 - Inverse distance weighting
-gs <- gstat(formula=pca~1, locations=~X+Y, data=data)
-idw <- interpolate(pust, gs, xyNames=c("X", "Y"), debug.level=0)
-idwmsk <- mask(idw, pus)
-plot(idwmsk, 1)
-points(datav,col=terrain.colors(10))
-rmse_idw <- rep(NA, 5)
-for (k in 1:5) {
-    test <- data[kf == k, ]
-    train <- data[kf != k, ]
-    gscv <- gstat(formula=pca~1, locations=~X+Y, data=train)
-    p <- predict(gscv, test, debug.level=0)$var1.pred
-    rmse_idw[k] <- RMSE(test$pca, p)
-}
-mean(rmse_idw)
-# relative model performance
-perf_idw <- 1 - (mean(rmse_idw) / null)
-round(perf_idw, 3)
-
-# Fit a variogram
-v <- variogram(gs,width=50000)
-v
-plot(v)
-
-# Optimization
-f1 <- function(x, test, train) {
-    nmx <- x[1]
-    idp <- x[2]
-    if (nmx < 1) return(Inf)
-    if (idp < .001) return(Inf)
-    m <- gstat(formula=pca~1, locations=~X+Y, data=train, nmax=nmx, set=list(idp=idp))
-    p <- predict(m, newdata=test, debug.level=0)$var1.pred
-    RMSE(test$pca, p)
-}
-set.seed(20150518)
-i <- sample(nrow(data), 0.2 * nrow(data))
-tst <- data[i,]
-trn <- data[-i,]
-opt <- optim(c(8, .5), f1, test=tst, train=trn)
-str(opt)
-
-# Best model
-m <- gstat(formula=pca~1, locations=~X+Y, data=data, nmax=opt$par[1], set=list(idp=opt$par[2]))
-idw <- interpolate(pust, m, xyNames=c("X", "Y"), debug.level=0)
-idwmsk <- mask(idw, pus)
-plot(idw,1)
-plot(idwmsk, 1)
-points(datav,col=terrain.colors(10))
-values(idwmsk) %>% as.vector %>% unique %>% length
+rmse_nn
+barplot(perf_nn)
+# we retain only perf > 0 (negative means the null model is better)
 
 
-# 3 - Inverse path distance weighting
-SeaRaster <- rast("C:/Users/marco/Desktop/seamap05.tif")
-plot(SeaRaster)
-plot(project(datav, crs(SeaRaster)),add=T)
-# Generate a stack of path accumulated distance raster objects
-datav %>% st_as_sf %>% 
-    # .[1:2,] %>%
-    st_transform(crs(SeaRaster))  -> data2
-# SeaRaster %>% project(data2) %>% as("Raster") -> sea_rast
-rstack <- pathdistGen(data2, as(SeaRaster,"Raster"), 100, progressbar = T)
-plot(rstack,20)
-plot(data2[20,],add=T)
 
-final.raster <- ipdwInterp(data2, rstack, paramlist = c("pca"), 
-                           dist_power = 0.5, overlapped = TRUE)
-plot(final.raster)
-plot(data2, add = TRUE)
-plot(data2)
-
-
-# riprendere da qui e fare sPCA??
-
-
-# # Create transition matrix and correct it for lat-long
-# t <- transition(SeaRaster, function(x) mean(x), directions=8)
-# t <- geoCorrection(t, type="c")
 # 
-# # Calculate least coast distances
-# datav %>% st_as_sf %>% st_geometry %>% st_transform(st_crs(4326)) %>% st_coordinates -> fromCoords
-# sea_dist <-  costDistance(t, fromCoords) 
-# ### guardare anche terra::costDist
-# sea_dist %>% as.matrix -> sea_dist
-# m2 <- rf_spatial(data=data, dependent.variable.name = "pca", predictor.variable.name = "dummy",
-#                  distance.matrix = sea_dist, method="mem.moran.sequential")
-
+# # 2 - Inverse distance weighting
+# gs <- gstat(formula=pca~1, locations=~X+Y, data=data)
+# idw <- interpolate(pust, gs, xyNames=c("X", "Y"), debug.level=0)
+# idwmsk <- mask(idw, pus)
+# plot(idwmsk, 1)
+# points(datav,col=terrain.colors(10))
+# rmse_idw <- rep(NA, 5)
+# for (k in 1:5) {
+#     test <- data[kf == k, ]
+#     train <- data[kf != k, ]
+#     gscv <- gstat(formula=pca~1, locations=~X+Y, data=train)
+#     p <- predict(gscv, test, debug.level=0)$var1.pred
+#     rmse_idw[k] <- RMSE(test$pca, p)
+# }
+# mean(rmse_idw)
+# # relative model performance
+#  1 - (mean(rmse_idw) / null)
+# round(perf_idw, 3)
+# 
+# # Fit a variogram
+# v <- variogram(gs,width=50000)
+# v
+# plot(v)
+# 
+# # Optimization
+# f1 <- function(x, test, train) {
+#     nmx <- x[1]
+#     idp <- x[2]
+#     if (nmx < 1) return(Inf)
+#     if (idp < .001) return(Inf)
+#     m <- gstat(formula=pca~1, locations=~X+Y, data=train, nmax=nmx, set=list(idp=idp))
+#     p <- predict(m, newdata=test, debug.level=0)$var1.pred
+#     RMSE(test$pca, p)
+# }
+# set.seed(20150518)
+# i <- sample(nrow(data), 0.2 * nrow(data))
+# tst <- data[i,]
+# trn <- data[-i,]
+# opt <- optim(c(8, .5), f1, test=tst, train=trn)
+# str(opt)
+# 
+# # Best model
+# m <- gstat(formula=pca~1, locations=~X+Y, data=data, nmax=opt$par[1], set=list(idp=opt$par[2]))
+# idw <- interpolate(pust, m, xyNames=c("X", "Y"), debug.level=0)
+# idwmsk <- mask(idw, pus)
+# plot(idw,1)
+# plot(idwmsk, 1)
+# points(datav,col=terrain.colors(10))
+# values(idwmsk) %>% as.vector %>% unique %>% length
+# 
+# 
+# # 3 - Inverse path distance weighting
+# library(ipdw)
+# SeaRaster <- rast("C:/Users/marco/Desktop/seamap05.tif")
+# plot(SeaRaster)
+# plot(project(datav, crs(SeaRaster)),add=T)
+# # Generate a stack of path accumulated distance raster objects
+# datav %>% st_as_sf %>% 
+#     # .[1:2,] %>%
+#     st_transform(crs(SeaRaster))  -> data2
+# # SeaRaster %>% project(data2) %>% as("Raster") -> sea_rast
+# rstack <- pathdistGen(data2, as(SeaRaster,"Raster"), 100, progressbar = T)
+# plot(rstack,20)
+# plot(data2[20,],add=T)
+# 
+# final.raster <- ipdwInterp(data2, rstack, paramlist = c("pca"), 
+#                            dist_power = 0.5, overlapped = TRUE)
+# plot(final.raster)
+# plot(data2, add = TRUE)
+# plot(data2)
+# 
+# 
